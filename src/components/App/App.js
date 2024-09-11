4142a1122f2031d63c99889bf4810d216a35eb84
Joshua M Smith <radjosh@gmail.com>
2024-09-11 20:39:01 +0000

    successfully returning json from go to react. woot.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,38 @@
+import React from 'react';
+
 function App() {
-    return (
-        <div className='wrapper'>
-            <h1>mmm</h1>
-        </div>
-    )
-}
+    const ENDPOINT = 
+        // 'http://localhost:3000/monster'
+        'http://localhost:8088/monster'
+
+        const [monster, setMonster] = React.useState('');
+
+        async function handleSubmit(event) {
+            // console.log(monster);
+            event.preventDefault();
+            const url = new URL(ENDPOINT);
+            url.searchParams.append('name', monster);
+            // console.log(url);
+            const response = await fetch(url, {
+                method: 'GET',
+            });
+            const json = await response.json();
+            console.log(json)
+        }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    value={monster}
+                    onChange={(event) => {
+                        setMonster(() => event.target.value);
+                        console.log(monster);
+                    }}
+                />
+                <input type="submit" value="submit" />
+            </form>
+        )
+    }
 
 export default App;
