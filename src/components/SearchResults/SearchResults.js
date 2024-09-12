ee0aa278f07cc8ef6b0a3e07c5f855dfa5024634
Joshua M Smith <radjosh@gmail.com>
2024-09-12 14:35:50 +0000

    displaying all results correctly

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -2,7 +2,21 @@ import React from "react";
 
 function SearchResults({ result }) {
   //   return <h1>OH FUCK</h1>;
-  return <h1>{result.Name}</h1>;
+  return (
+    <div className="results">
+      <h1>{result.Name.toUpperCase()}</h1>
+      <h1>SIZE: {result.Size}</h1>
+      <h1>ALIGNMENT: {result.Alignment}</h1>
+      <h1>AC: {result.Ac}</h1>
+      <h1>HP: {result.Hp}</h1>
+      <h1>STR: {result.Str}</h1>
+      <h1>DEX: {result.Dex}</h1>
+      <h1>CON: {result.Con}</h1>
+      <h1>INT: {result.Int}</h1>
+      <h1>WIS: {result.Wis}</h1>
+      <h1>CHA: {result.Cha}</h1>
+    </div>
+  );
 }
 
 export default SearchResults;
