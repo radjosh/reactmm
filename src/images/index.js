df2b393306ecaab6b1b17d4f3a36ed860993c2ba
Joshua M Smith <radjosh@gmail.com>
2024-09-13 20:01:16 +0000

    tracking monster images and index.js for dynamic import

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+const images = {
+  cloudgiant: require("./cloudgiant.webp"),
+  firegiant: require("./firegiant.webp"),
+  frostgiant: require("./frostgiant.webp"),
+  hillgiant: require("./hillgiant.webp"),
+  mindflayer: require("./mindflayer.webp"),
+  stormgiant: require("./stormgiant.webp"),
+  succubus: require("./succubus.webp"),
+};
+
+export default images;
