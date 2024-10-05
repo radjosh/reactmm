d81c15e39076414c04a50f6a63c0fdd7a7a3f776
Joshua M Smith <radjosh@gmail.com>
2024-10-05 19:56:56 +0000

    missed a few columns/stats

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -18,15 +18,18 @@ function SearchResults({ result }) {
         <div className="results">
           <h1>{result.Name.toUpperCase()}</h1>
           <h2>SIZE: {result.Size}</h2>
+          <h2>TYPE: {result.Type}</h2>
           <h2>ALIGNMENT: {result.Alignment}</h2>
           <h2>AC: {result.Ac}</h2>
           <h2>HP: {result.Hp}</h2>
+          <h2>SPEED: {result.Speed}</h2>
           <h2>STR: {result.Str}</h2>
           <h2>DEX: {result.Dex}</h2>
           <h2>CON: {result.Con}</h2>
           <h2>INT: {result.Int}</h2>
           <h2>WIS: {result.Wis}</h2>
           <h2>CHA: {result.Cha}</h2>
+          {result.Save ? <h2>SAVE: {result.Save}</h2> : null}
           {result.Skill ? <h2>SKILL: {result.Skill}</h2> : null}
           <h2>PASSIVE PERCEPTION: {result.Passive}</h2>
           <h2>LANGUAGES: {result.Languages}</h2>
@@ -34,8 +37,10 @@ function SearchResults({ result }) {
           {result.Senses ? <h2>SENSES: {result.Senses}</h2> : null}
           {result.Vulnerable ? <h2>VULNERABLE: {result.Vulnerable}</h2> : null}
           {result.Immune ? <h2>IMMUNE: {result.Immune}</h2> : null}
+          {result.ConditionImmune ? (
+            <h2>CONDITION IMMUNITIES: {result.ConditionImmune}</h2>
+          ) : null}
           {result.Resist ? <h2>RESISTANT: {result.Resist}</h2> : null}
-          {result.Spells ? <h2>SPELLS: {result.Spells}</h2> : null}
           {result.Traits?.map((trait) => (
             <div key={trait.name + result.name}>
               <h2>TRAIT: {trait.name}</h2>
@@ -58,6 +63,17 @@ function SearchResults({ result }) {
               ) : null}
             </div>
           ))}
+          {result.Reactions?.map((reaction) => (
+            <div key={reaction.name + result.name}>
+              <h2>REACTION: {reaction.name}</h2>
+              <h2 className="traits"> {reaction.text}</h2>
+              {reaction.attack ? (
+                <h2 className="traits" style={{ color: "red" }}>
+                  {reaction.attack}
+                </h2>
+              ) : null}
+            </div>
+          ))}
           {result.Legendary?.map((legendary) => (
             <div key={legendary.name + result.name}>
               <h2>LEGENDARY: {legendary.name}</h2>
@@ -69,6 +85,8 @@ function SearchResults({ result }) {
               ) : null}
             </div>
           ))}
+          {result.Spells ? <h2>SPELLS: {result.Spells}</h2> : null}
+          {result.Slots ? <h2>SLOTS: {result.Slots}</h2> : null}
           {result.Description ? (
             <div>
               <br />
